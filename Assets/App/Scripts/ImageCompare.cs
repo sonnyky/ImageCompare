fbff8097c3a75352f4e48d14ee1c9585dc7bbf8a
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2019-08-30 12:05:20 +0900

    Packman

--- Assets/App/Scripts/ImageCompare.cs
+++ Assets/App/Scripts/ImageCompare.cs
@@ -12,47 +12,7 @@ public class ImageCompare : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        m_ImageFilePaths = Directory.GetFiles(m_ImagesPath, "*.jpg");
-
-        Debug.Log("Number of images : " + m_ImageFilePaths.Length);
-
-        byte[] imageA = File.ReadAllBytes(m_ImageFilePaths[2]);
-        Texture2D texA = new Texture2D(320, 240);
-        texA.LoadImage(imageA);
-        Texture2D resizedA = Resize(texA, 16, 16);
-        Color[] colorsA = resizedA.GetPixels();
-
-        for (int i = 0; i < m_ImageFilePaths.Length; i++) {
-            byte[] imageB = File.ReadAllBytes(m_ImageFilePaths[i]);
-
-            Texture2D texB = new Texture2D(320, 240);
-            texB.LoadImage(imageB);
-            Texture2D resizedB = Resize(texB, 16, 16);
-            Color[] colorsB = resizedB.GetPixels();
-
-
-            float diff = 0;
-
-            for (int j = 0; j < colorsA.Length; j++)
-            {
-                Color color = colorsB[j] - colorsA[j];
-                diff += Mathf.Sqrt((color.r * color.r) + (color.g * color.g) + (color.b * color.b));
-            }
-
-            float score = diff / 256;
-            Debug.Log("Final score against : " + i + " is : " + score);
-        }
-    }
-
-    void ResizeAndSave(string imgPath)
-    {
-        byte[] image = File.ReadAllBytes(imgPath);
-        Texture2D tex = new Texture2D(320, 240);
-        tex.LoadImage(image);
-        Texture2D resized = Resize(tex, 16, 12);
-
-        var data = resized.EncodeToPNG();
-        System.IO.File.WriteAllBytes(m_ImagesPath + "/resized.png", data);
+       
     }
 
     public static float CompareImages(string imA, string imB)
@@ -95,6 +55,5 @@ public class ImageCompare : MonoBehaviour {
         nTex.Apply();
         RenderTexture.active = null;
         return nTex;
-
     }
 }
